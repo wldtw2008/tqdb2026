a7dec9c9f1ac00801d7687e64c91b7e395dd3305
PeterShih <petershih@email.com>
2026-08-04 09:47:33 +0800

    modify start sh

--- host/cassandra_start.sh
+++ host/cassandra_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-podman run -d \
+podman run -d --rm \
     --name cassandra \
     -p 9042:9042 \
-    -v ../cassandra.data:/var/lib/cassandra:Z \
+    -v $(pwd)/../cassandra.data:/var/lib/cassandra:Z \
     cassandra:4.1.11
--- host/tqdb_start.sh
+++ host/tqdb_start.sh
@@ -8,9 +8,9 @@ podman run -d --rm --name tqdb2026  \
 	--hostname TQDB2026 \
 	--security-opt=label=disable \
 	-p 8080:80 \
-	-v ../tqdb2026.files/tqdb2026:/tqdb2026 \
-	-v ../tqdb2026.oldtick/:/tqdb2026/oldtick \
-	-v ../tqdb2026.files/etc/crontab:/etc/crontab \
-	-v ../tqdb2026.files/etc/TQDB.vhost.conf:/etc/httpd/conf.d/TQDB.vhost.conf \
-	-v ../tqdb2026.files/www:/var/www \
+	-v $(pwd)/../tqdb2026.files/tqdb2026:/tqdb2026 \
+	-v $(pwd)/../tqdb2026.oldtick/:/tqdb2026/oldtick \
+	-v $(pwd)/../tqdb2026.files/etc/crontab:/etc/crontab \
+	-v $(pwd)/../tqdb2026.files/etc/TQDB.vhost.conf:/etc/httpd/conf.d/TQDB.vhost.conf \
+	-v $(pwd)/../tqdb2026.files/www:/var/www \
 	tqdb2026:rhel8 
